5b1866b17de9ac9de7018a520450e2f1794e14d3
oxeDNich <121075778+oxeDLie@users.noreply.github.com>
2023-12-20 21:33:14 +0800

    First Of Start

---
+++ ProjectPBO/src/Koneksi/Koneksi.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Koneksi;
+
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author user
+ */
+public class Koneksi {
+    private static java.sql.Connection koneksi;
+    
+    public static java.sql.Connection getKoneksi(){
+        if (koneksi==null){
+            try{
+                String url = "jdbc:mysql://localhost:3306/katanya";
+                String user = "root";
+                String pass = "";
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url,user,pass);
+                System.out.println("koneksi sukses");
+            }catch (Exception e){
+                System.out.println("Error");
+            }
+        }
+        return koneksi;
+    }
+    public static void main(String[] args) {
+        getKoneksi();
+    }
+}
